f01302b538c4d98ed5a229f5056fc6f8669efa38
Pete <p.eisenschmidt@gmail.com>
2021-02-21 09:34:47 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='WodPlanner',
+    version='0.0.1',
+    packages=['WodPlanner'],
+    url='',
+    license='',
+    author='Peter Eisenschmidt',
+    author_email='p.eisenschmidt@gmail.com',
+    description=''
+)
