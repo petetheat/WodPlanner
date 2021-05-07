bd669eb904f83831fdc0057fa92a3671313c212e
Pete <p.eisenschmidt@gmail.com>
2021-05-07 13:42:00 +0200

    Changed to sqlite

--- WodPlanner/wodplanner.py
+++ WodPlanner/wodplanner.py
@@ -2,36 +2,51 @@ import os
 import pandas as pd
 import numpy as np
 import warnings
+import sqlite3
+import json
 
 
 class WodPlanner:
     def __init__(self, new_wod=False):
         current_path = os.path.dirname(os.path.abspath(__file__))
-        self.db_name = os.path.join(current_path, 'db/movements.h5')
+        self.db_name = os.path.join(current_path, 'db/wodplanner.db')
         self.wod_name = os.path.join(current_path, 'db/wods.h5')
 
         if new_wod:
-            self.wod = self._initialize_wod_db()
+            self.db, self.c = self._initialize_wod_db()
         else:
-            self.wod = self._load_wod_db()
+            self.db, self.c = self._load_wod_db()
 
-        self.movement_dict = {}
-        self.df_g, self.df_w, self.df_m = self._load_movements()
+        self.movement_dict = {'g': 'Gymnastics', 'w': 'Weightlifting', 'm': 'Monostructural'}
 
     def _initialize_wod_db(self):
-        columns = ['Date', 'Strength/Skill', 'Sets_reps', 'Movement', 'Scheme',
-                   'Time_rounds', 'Wod_Movements', 'Reps', 'Weight', 'Comments']
+        conn = sqlite3.connect(self.db_name)
+        c = conn.cursor()
 
-        df = pd.DataFrame(columns=columns)
-        df.to_hdf(self.wod_name, key='wod')
+        c.execute("select name from sqlite_master where type = 'table';")
+        tables = c.fetchall()
 
-        return df
+        if ('wods',) in tables:
+            c.execute("DROP TABLE wods")
+
+        c.execute("CREATE TABLE wods (wod_id int, data json)")
+
+        return conn, c
 
     def _load_wod_db(self):
-        return pd.read_hdf(self.wod_name)
+        conn = sqlite3.connect(self.db_name)
+        c = conn.cursor()
+
+        c.execute("select name from sqlite_master where type = 'table';")
+        tables = c.fetchall()
+
+        if ('wods',) not in tables:
+            c.execute("CREATE TABLE wods (wod_id int, data json)")
+
+        return conn, c
 
     def _add_wod(self, date, strength_skill, sets_reps, movement, scheme, time_rounds, wod_movements, reps,
-                 weights, comments=None):
+                 weights, comments=''):
 
         wod = {'Date': date,
                'Strength/Skill': strength_skill,
@@ -44,60 +59,32 @@ class WodPlanner:
                'Weight': weights,
                'Comments': comments}
 
-        self.wod = pd.concat([self.wod, pd.json_normalize(wod)])
-        # self.wod['Date'] = pd.to_datetime(self.wod['Date'], format='%d/%m/%Y')
+        mydates = self.c.execute("select json_extract(data, '$.Date') from wods").fetchall()
+        mydates = [x[0] for x in mydates]
 
-        self.wod.to_hdf(self.wod_name, key='wod')
-
-    def _load_movements(self):
-        df_g = pd.read_hdf(self.db_name, 'g')
-        df_w = pd.read_hdf(self.db_name, 'w')
-        df_m = pd.read_hdf(self.db_name, 'm')
-
-        self.movement_dict['g'] = df_g['Type'].unique()[0]
-        self.movement_dict['w'] = df_w['Type'].unique()[0]
-        self.movement_dict['m'] = df_m['Type'].unique()[0]
-
-        return df_g, df_w, df_m
+        if date in mydates:
+            warnings.warn('Date already exists. Workout is not added.')
+        else:
+            self.c.execute("insert into wods values (?, ?)", [1, json.dumps(wod)])  # TODO: change wod ID
+            self.db.commit()
 
     def _add_movement(self, movement_name: str, key: str):
-        if key == 'g':
-            df = self.df_g
-        elif key == 'm':
-            df = self.df_m
-        elif key == 'w':
-            df = self.df_w
-        else:
+        if key not in self.movement_dict.keys():
             raise ValueError('Incorrect key %s' % key)
 
-        if movement_name in df['Movement'].values:
-            warnings.warn("%s is already included in the database" % movement_name)
+        self.c.execute('SELECT MAX(id) from movements;')
+        idx_max = self.c.fetchall()[0][0]
+
+        self.c.execute("SELECT COUNT(1) FROM movements WHERE Movement = '%s';" % movement_name)
+        movement_count = self.c.fetchall()[0][0]
+
+        if movement_count == 0:
+            self.c.execute("insert into movements values (?, ?, ?)", [idx_max + 1, movement_name,
+                                                                      self.movement_dict[key]])
+            self.db.commit()
         else:
-            columns = df.columns
-            data = np.array([movement_name, self.movement_dict[key]])
-            data = data.reshape(1, -1)
+            warnings.warn("%s is already included in the database and cannot be added" % movement_name)
 
-            df_new = pd.DataFrame(data=data, columns=columns)
-            df = pd.concat([df, df_new])
-            df.reset_index(drop=True).to_hdf(self.db_name, key=key)
-
-            self.df_g, self.df_w, self.df_m = self._load_movements()
-
-    def _drop_movement(self, movement_name: str, key: str):
-        if key == 'g':
-            df = self.df_g
-        elif key == 'm':
-            df = self.df_m
-        elif key == 'w':
-            df = self.df_w
-        else:
-            raise ValueError('Incorrect key %s' % key)
-
-        if movement_name in df['Movement'].values:
-            idx = df['Movement'] == movement_name
-            df = df.loc[~idx].reset_index(drop=True)
-            df.to_hdf(self.db_name, key=key)
-
-            self.df_g, self.df_w, self.df_m = self._load_movements()
-        else:
-            warnings.warn("%s is not included in the database and cannot be removed" % movement_name)
+    def _drop_movement(self, movement_name: str):
+        self.c.execute("DELETE FROM movements WHERE Movement='%s';" % movement_name)
+        self.db.commit()
