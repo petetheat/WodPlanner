6ec35938441cdaa2926fbd2eb259935f4f7a92c4
Pete <p.eisenschmidt@gmail.com>
2021-05-08 09:05:08 +0200

    Schema added; h5 files removed

--- WodPlanner/wodplanner.py
+++ WodPlanner/wodplanner.py
@@ -10,7 +10,6 @@ class WodPlanner:
     def __init__(self, new_wod=False):
         current_path = os.path.dirname(os.path.abspath(__file__))
         self.db_name = os.path.join(current_path, 'db/wodplanner.db')
-        self.wod_name = os.path.join(current_path, 'db/wods.h5')
 
         if new_wod:
             self.db, self.c = self._initialize_wod_db()
@@ -19,7 +18,15 @@ class WodPlanner:
 
         self.movement_dict = {'g': 'Gymnastics', 'w': 'Weightlifting', 'm': 'Monostructural'}
 
+        self.movements, self.schemas = self._get_data()
+
     def _initialize_wod_db(self):
+        """
+        Loads sqlite database with existing movements and schemas. If the WODs table exists, it is overwritten by an
+        empty one.
+
+        :return: :obj:`conn` (sqlite3 connector), :obj:`c` (sqlite3 cursor)
+        """
         conn = sqlite3.connect(self.db_name)
         c = conn.cursor()
 
@@ -34,6 +41,11 @@ class WodPlanner:
         return conn, c
 
     def _load_wod_db(self):
+        """
+        Loads sqlite database with existing WODs. If WODs table is empty a new one is created.
+
+        :return: :obj:`conn` (sqlite3 connector), :obj:`c` (sqlite3 cursor)
+        """
         conn = sqlite3.connect(self.db_name)
         c = conn.cursor()
 
@@ -45,9 +57,30 @@ class WodPlanner:
 
         return conn, c
 
+    def _get_data(self):
+        self.c.execute('SELECT Movement, Type from movements;')
+        movements = dict(self.c.fetchall())
+
+        self.c.execute('SELECT Schema from schemas;')
+        schemas = [x[0] for x in self.c.fetchall()]
+
+        return movements, schemas
+
     def _add_wod(self, date, strength_skill, sets_reps, movement, scheme, time_rounds, wod_movements, reps,
                  weights, comments=''):
+        """
 
+        :param date:
+        :param strength_skill:
+        :param sets_reps:
+        :param movement:
+        :param scheme:
+        :param time_rounds:
+        :param wod_movements:
+        :param reps:
+        :param weights:
+        :param comments:
+        """
         wod = {'Date': date,
                'Strength/Skill': strength_skill,
                'Sets_reps': sets_reps,
@@ -82,9 +115,11 @@ class WodPlanner:
             self.c.execute("insert into movements values (?, ?, ?)", [idx_max + 1, movement_name,
                                                                       self.movement_dict[key]])
             self.db.commit()
+            self.movements, self.schemas = self._get_data()
         else:
             warnings.warn("%s is already included in the database and cannot be added" % movement_name)
 
     def _drop_movement(self, movement_name: str):
         self.c.execute("DELETE FROM movements WHERE Movement='%s';" % movement_name)
         self.db.commit()
+        self.movements, self.schemas = self._get_data()
